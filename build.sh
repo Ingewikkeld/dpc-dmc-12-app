6ff49ec9eaad8943b1d281d6e54b01f58efd132b
Martin de Keijzer <martin.dekeijzer@gmail.com>
2012-10-18 15:32:30 +0200

    removed references to the iOS and Android distributions as these are not part of the package

--- build.sh
+++ build.sh
@@ -29,33 +29,4 @@ mv resources/icons_orig resources/icons
 mv resources/loading resources/loading_dpc
 mv resources/loading_orig resources/loading
 
-# DPC iOS deploy
-echo "Deploying the dpc iOS package"
-rm -rf ../dpc/www
-cp -R ../www_dpc ../dpc/www
-
-#DPC Android deploy
-echo "Deploying the dpc Android package"
-rm -rf ../android_dpc/assets/www
-cp -R ../www_dpc ../android_dpc/assets/www
-
-# DMC iOS deploy
-echo "Deploying the dmc iOS package"
-rm -rf ../dmc/www
-cp -R ../www_dmc ../dmc/www
-
-#DMC Android deploy
-echo "Deploying the DMC Android package"
-rm -rf ../android_dmc/assets/www
-cp -R ../www_dmc ../android_dmc/assets/www
-
 mv appold.js app.js
-
-#Right Cordova JS copy
-echo "Copying cordova js to the right deployments"
-cp resources/lib/cordova-1.7.0_android.js ../android_dmc/assets/www/resources/lib/cordova-1.7.0.js
-cp resources/lib/cordova-1.7.0_android.js ../android_dpc/assets/www/resources/lib/cordova-1.7.0.js
-cp resources/lib/cordova-1.7.0_ios_www.js ../dmc/www/resources/lib/cordova-1.7.0.js
-cp resources/lib/cordova-1.7.0_ios_www.js ../dpc/www/resources/lib/cordova-1.7.0.js
-cp resources/lib/cordova-1.7.0_ios_www.js ../www_dmc/resources/lib/cordova-1.7.0.js
-cp resources/lib/cordova-1.7.0_ios_www.js ../www_dpc/resources/lib/cordova-1.7.0.js
